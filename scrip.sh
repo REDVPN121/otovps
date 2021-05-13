b0157148380235930ff133159fad5690f8822d5e
vyner-stack <67986263+vyner-stack@users.noreply.github.com>
2021-05-14 02:29:38 +0800

    Update scrip.sh

--- scrip.sh
+++ scrip.sh
@@ -65,6 +65,7 @@ PLEASE WAIT TAKE TIME 1-5 MINUTE
 "
 sh -c 'echo "deb https://download.webmin.com/download/repository sarge contrib" > /etc/apt/sources.list.d/webmin.list'
 wget -qO - https://download.webmin.com/jcameron-key.asc | apt-key add jcameron-key.asc
+apt-get install apt-transport-https;
 apt-get update;
 apt-get -y autoremove;
 apt-get -y install wget curl;
